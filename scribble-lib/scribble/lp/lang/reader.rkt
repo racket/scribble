8e4e2e3405fa5eae17da4bc1ca2071d44951ea99
Matthew Flatt <mflatt@racket-lang.org>
2014-12-15 18:18:23 -0700

    revert enabling of DrRacket's Scribble buttons for `scribble/lp`
    The buttons should be enabled for `scribble/lp2`, only.

--- scribble-lib/scribble/lp/lang/reader.rkt
+++ scribble-lib/scribble/lp/lang/reader.rkt
@@ -5,11 +5,13 @@ scribble/lp/lang/lang
 #:read read-inside
 #:read-syntax read-syntax-inside
 #:whole-body-readers? #t
+;; don't use scribble-base-info for the #:info arg, since
+;; scribble/lp files are not directly scribble'able.
 #:language-info (scribble-base-language-info)
-#:info (scribble-base-info)
+#:info (scribble-base-reader-info)
 (require scribble/reader
          (only-in scribble/base/reader
-                  scribble-base-info
+                  scribble-base-reader-info
                   scribble-base-language-info))
 
 
