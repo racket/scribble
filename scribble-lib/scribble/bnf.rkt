a2d2f0472f09987e4d070ddff7cc4bfb26e91e9d
Philip McGrath <philip@philipmcgrath.com>
2018-04-15 05:27:17 -0500

    scribble/bnf: Add contracts & correct docs

--- scribble-lib/scribble/bnf.rkt
+++ scribble-lib/scribble/bnf.rkt
@@ -1,19 +1,45 @@
 (module bnf racket
-  (require "struct.rkt"
-           "decode.rkt"
-           (only-in "core.rkt" 
+  (require scribble/decode
+           (except-in scribble/struct 
+                      element?)
+           (only-in scribble/core
+                    content?
+                    element?
                     make-style
                     make-table-columns)
-           mzlib/kw)
+           )
 
-  (provide BNF 
-           nonterm
-           BNF-seq BNF-seq-lines
-           BNF-alt  BNF-alt/close ; single-line alternatives
-           BNF-etc
-           BNF-group
-           optional kleenestar kleeneplus kleenerange)
-
+  (provide (contract-out
+            [BNF (-> (cons/c (or/c block? content?)
+                             (non-empty-listof (or/c block? content?)))
+                     ... 
+                     table?)]
+            [BNF-etc element?]
+            ;; operate on content
+            [BNF-seq (-> content? ... 
+                         (or/c element? ""))]
+            [BNF-seq-lines (-> (listof content?) ... 
+                               block?)]
+            [BNF-alt (-> content? ...
+                         element?)]
+            [BNF-alt/close (-> content? ...
+                               element?)]
+            ;; operate on pre-content
+            [BNF-group (-> pre-content? ...
+                           element?)]
+            [nonterm (-> pre-content? ...
+                         element?)]
+            [optional (-> pre-content? ...
+                          element?)]
+            [kleenestar (-> pre-content? ...
+                            element?)]
+            [kleeneplus (-> pre-content? ...
+                            element?)]
+            [kleenerange (-> any/c any/c pre-content? ...
+                             element?)]
+            ))
+  
+  
   (define spacer (make-element 'hspace (list " ")))
   (define equals (make-element 'tt (list spacer "::=" spacer)))
   (define alt (make-element 'tt (list spacer spacer "|" spacer spacer)))
@@ -33,14 +59,16 @@
                    (list baseline baseline baseline baseline))))
      (apply
       append
-      (map (lambda (defn)
-             (cons
-              (list (as-flow spacer) (as-flow (car defn)) (as-flow equals) (as-flow (cadr defn)))
-              (map (lambda (i)
-                     (list (as-flow spacer) (as-flow " ") (as-flow alt) (as-flow i)))
-                   (cddr defn))))
+      (map (match-lambda
+             [(cons lhs (cons rhs0 more-rhs))
+              (cons
+               (list (as-flow spacer) (as-flow lhs) (as-flow equals) (as-flow rhs0))
+               (map (lambda (i)
+                      (list (as-flow spacer) (as-flow " ") (as-flow alt) (as-flow i)))
+                    more-rhs))])
            defns))))
 
+  ;; interleave : (listof content?) element? -> element?
   (define (interleave l spacer)
     (make-element #f (cons (car l)
                            (apply append
@@ -65,28 +93,28 @@
 
   (define BNF-etc (make-element 'roman "..."))
 
-  (define/kw (nonterm #:body s)
+  (define (nonterm . s)
     (make-element 'roman (append (list 'lang)
                                  (list (make-element 'italic (decode-content s)))
                                  (list 'rang))))
 
-  (define/kw (optional #:body s)
+  (define (optional . s)
     (make-element #f (append (list (make-element 'roman "["))
                              (decode-content s)
                              (list (make-element 'roman "]")))))
 
-  (define/kw (BNF-group #:body s)
+  (define (BNF-group . s)
     (make-element #f (append (list (make-element 'roman "{"))
                              (list (apply BNF-seq (decode-content s)))
                              (list (make-element 'roman "}")))))
 
-  (define/kw (kleenestar #:body s)
+  (define (kleenestar . s)
     (make-element #f (append (decode-content s) (list (make-element 'roman "*")))))
 
-  (define/kw (kleeneplus #:body s)
+  (define (kleeneplus . s)
     (make-element #f (append (decode-content s) (list (make-element 'superscript (list "+"))))))
 
-  (define/kw (kleenerange a b #:body s)
+  (define (kleenerange a b . s)
     (make-element #f (append (decode-content s) 
                              (list (make-element 'roman 
                                                  (make-element 'superscript 
