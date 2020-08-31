503aa526e266f48f4d31205b27bbbef8d648f39e
Jay McCarthy <jay.mccarthy@gmail.com>
2020-08-31 16:44:33 -0400

    add current-html-render-head-prefix

--- scribble-lib/scribble/html-render.rkt
+++ scribble-lib/scribble/html-render.rkt
@@ -25,7 +25,11 @@
          "search.rkt"
          (except-in "base.rkt" url))
 (provide render-mixin
-         render-multi-mixin)
+         render-multi-mixin
+         current-html-render-head-prefix)
+
+(define current-html-render-head-prefix
+  (make-parameter '()))
 
 (define (number->decimal-string s)
   (number->string (if (integer? s) s (exact->inexact s))))
@@ -872,6 +876,7 @@
             (xml:write-xexpr
               `(html ,(style->attribs (part-style d))
                  (head ()
+                   ,@(current-html-render-head-prefix)
                    (meta ([http-equiv "content-type"]
                           [content "text/html; charset=utf-8"]))
                    (meta ([name "viewport"]
